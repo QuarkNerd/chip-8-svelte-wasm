24d63fa18f49ab1a5836ac0af68594cdceb92363
Gurveer Arora <garora1410@yahoo.co.uk>
2021-04-21 21:22:05 +0100

    create base Emulator struct in rust

--- src/wasm/src/lib.rs
+++ src/wasm/src/lib.rs
@@ -1,10 +1,33 @@
 use wasm_bindgen::prelude::*;
+use js_sys::Uint8Array;
+use display::*;
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn greet() -> String {
-    "aaaaaaaaaaa SE from RUST".into()
+pub struct Emulator {
+    display: Display
 }
+
+#[wasm_bindgen]
+impl Emulator {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Emulator {
+        Emulator {
+            display: Display::new()
+        }
+    }
+
+    pub fn get_display_ptr(&self) -> Uint8Array {
+        self.display.cells()
+    }
+}
+
+#[wasm_bindgen]
+pub fn get_memory() -> JsValue {
+    wasm_bindgen::memory()
+}
+
+mod display;
