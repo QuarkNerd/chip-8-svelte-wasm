09a36bcb14f2a74583ee240e3da8ac3f422722f7
Gurveer Arora <garora1410@yahoo.co.uk>
2021-04-21 21:23:08 +0100

    create emulator in JS

---
+++ src/Emulator.svelte
@@ -0,0 +1,25 @@
+<script lang="typescript">
+  import { onMount } from "svelte";
+  
+  const COLS: number = 64;
+  const ROWS: number = 32;
+  
+  export let scale: number;
+  let canvas: HTMLCanvasElement;
+
+  onMount(() => {
+    const ctx = canvas.getContext("2d");
+    canvas.width = scale * COLS;
+    canvas.height = scale * ROWS;
+  });
+</script>
+
+<main>
+  <canvas bind:this={canvas} />
+</main>
+
+<style>
+  canvas {
+    border: 2px solid black;
+  }
+</style>
