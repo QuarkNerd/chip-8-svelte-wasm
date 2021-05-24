ff1d7e1bd0deace3b9be815fc6c2a098d512e913
Gurveer Arora <garora1410@yahoo.co.uk>
2021-05-24 20:51:38 +0100

    Create game holder

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,45 @@
 <script lang="ts">
   import Emulator from "./Emulator.svelte";
-  import GameCartriage from "./GameCartriage.svelte";
+  import GamesHolder from "./GamesHolder.svelte";
+
+  const games = [
+    "15PUZZLE",
+    "BLINKY",
+    "BLITZ",
+    "BRIX",
+    "CONNECT4",
+    "GUESS",
+    "HIDDEN",
+    "IBM",
+    "INVADERS",
+    "KALEID",
+    "MAZE",
+    "MERLIN",
+    "MISSILE",
+    "PONG",
+    "PONG2",
+    "PUZZLE",
+    "SYZYGY",
+    "TANK",
+    "TETRIS",
+    "TICTAC",
+    "UFO",
+    "VBRIX",
+    "VERS",
+    "WIPEOFF",
+  ];
 </script>
 
 <main>
   <Emulator />
+  <GamesHolder {games} />
 </main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin: 10px auto;
+  }
+</style>
---
+++ src/GamesHolder.svelte
@@ -0,0 +1,31 @@
+<script lang="typescript">
+  import GameCartriage from "./GameCartriage.svelte";
+  export let games: string[] = [];
+</script>
+
+<main>
+  {#each games as game}
+    <GameCartriage name={game} colour="blue"/>
+  {/each}
+</main>
+
+<style>
+  main {
+    position: relative;
+    margin: 0 5px;
+    padding: 20px 0;
+    height: 400px;
+    width: 280px;
+    border-radius: 35px;
+    flex-shrink: 0;
+
+    background-color: lightgray;
+    box-shadow: rgb(0 0 0) -4px 6px 10px,
+      rgb(255 255 255 / 25%) -6px 8px 6px inset;
+
+    overflow: hidden;
+
+    display: flex;
+    flex-flow: row wrap;
+  }
+</style>
